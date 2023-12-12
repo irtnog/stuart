11865d631c2813fe8bf2bbc0c77202f1640e1a1e
Matthew X. Economou <xenophon@irtnog.org>
2023-12-12 17:16:17 -0500

    test(app): rename test to better reflect its scope

--- tests/test_app.py
+++ tests/test_app.py
@@ -18,7 +18,7 @@
 from stuart.app import create_app
 
 
-def test_home_page():
+def test_app():
     with create_app().test_client() as test_client:
         response = test_client.get("/")
         assert response.status_code == 200
