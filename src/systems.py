41b4abc131dd482f6ce4a53d029754d5a401c681
Matthew X. Economou <xenophon@irtnog.org>
2024-09-07 13:05:36 -0400

    feat(systems): outline the interface to system data

---
+++ src/systems.py
@@ -0,0 +1,31 @@
+# stuart, a self-hostable game data aggregator for Elite: Dangerous
+# Copyright (C) 2023  Matthew X. Economou
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with this program.  If not, see
+# <https://www.gnu.org/licenses/>.
+
+import logging
+
+from flask import Blueprint, render_template
+
+# configure module-level logging
+logger = logging.getLogger(__name__)
+
+url_prefix = "/".join(__name__.split(".")[1:]).replace("_", "-")
+bp = Blueprint(__name__.split(".")[-1], __name__, url_prefix=f"/{url_prefix}")
+
+
+@bp.route("/")
+def index():
+    return render_template(f"{url_prefix}/index.html")
